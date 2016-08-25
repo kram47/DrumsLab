eead55454e0cf1f956ad8504781d89065078c051
Marc CHARTON <mcn@photomaton.fr>
2016-08-25 18:33:41 +0200

    - Compréhension de vexflow et test d'écriture de partition  - WIP : tentative de dessin de triolet

--- src/poc/vexflow/main.js
+++ src/poc/vexflow/main.js
@@ -1,7 +1,7 @@
 
     VF = Vex.Flow;
 
-    // Create an SVG renderer and attach it to the DIV element named "boo".
+    // Create an SVG renderer and attach it to the DIV element named "vexflow_container".
     var div = document.getElementById("vexflow_container")
     var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
@@ -14,33 +14,51 @@
     var stave = new VF.Stave(10, 40, 400);
 
     // Add a clef and time signature.
-    stave.addClef("treble").addTimeSignature("4/4");
+    stave.addClef("percussion").addTimeSignature("4/4");
 
     // Connect it to the rendering context and draw!
     stave.setContext(context).draw();
 
 
     var notes = [
-      // A quarter-note C.
-      new VF.StaveNote({ keys: ["c/5"], duration: "e" }),
-
-      // A quarter-note D.
-      new VF.StaveNote({ keys: ["d/4"], duration: "q" }),
-
-      // A quarter-note rest. Note that the key (b/4) specifies the vertical
-      // position of the rest.
-      new VF.StaveNote({ keys: ["b/4"], duration: "qr" }),
-
-      // A C-Major chord.
-      new VF.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" })
+      new VF.StaveNote({ keys: ["c/5"], duration: "8" }),
+      new VF.StaveNote({ keys: ["c/5"], duration: "16" }),
+      new VF.StaveNote({ keys: ["c/5"], duration: "16" }),
+      new VF.StaveNote({ keys: ["c/5"], duration: "8r" }),
+      new VF.StaveNote({ keys: ["c/5"], duration: "8" }),
+      new VF.StaveNote({ keys: ["c/5"], duration: "16" }),
+      new VF.StaveNote({ keys: ["c/5"], duration: "8" }),
+      new VF.StaveNote({ keys: ["c/5"], duration: "16" })
     ];
 
-    // Create a voice in 4/4 and add above notes
-    var voice = new VF.Voice({num_beats: 4,  beat_value: 4});
-    voice.addTickables(notes);
+    var notes2 = [
+      new VF.StaveNote({ keys: ["c/5"], duration: "8" }),
+      new VF.StaveNote({ keys: ["c/5"], duration: "8" }),
+      new VF.StaveNote({ keys: ["c/5"], duration: "8" })
+    ];
 
-    // Format and justify the notes to 400 pixels.
-    var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
 
-    // Render voice
-    voice.draw(context, stave);
+
+    var beams = VF.Beam.generateBeams(notes, {
+        stem_direction: 1
+    });
+
+    // Now create the tuplets:
+    var triplet = new Vex.Flow.Tuplet(notes2, {
+      num_notes: 3, notes_occupied: 2
+    });
+    Vex.Flow.Formatter.FormatAndDraw(context, stave, notes);
+    beams.forEach(function(b) {b.setContext(context).draw()})
+
+    triplet.setContext(ctx).draw();
+
+    var ties = [
+      new VF.StaveTie({
+        first_note: notes2[1],
+        last_note: notes3[0],
+        first_indices: [0],
+        last_indices: [0]
+      })
+    ];
+    ties.forEach(function(t) {t.setContext(context).draw()})
+
