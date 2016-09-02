cf3bbe00c3094b512509eb94e2b48f361fa2f547
Marc CHARTON <mcn@photomaton.fr>
2016-09-02 11:30:37 +0200

    - Utilisation du module "Score"  - Application du "Module Pattern" pour Score

--- src/js/app.js
+++ src/js/app.js
@@ -9,115 +9,186 @@
  *
  */
 
-var app = app || {};
-
-(function (self){
+var app = (function (self){
 
     "use strict";
-    var _name_ = "App";
 
-    /*
-        Initialisation du module
-     */
-    self.init = function()
-    {
-        console.log("["+_name_+"] " + "Initialisation");
+    // -- -- M O D U L E   S E T T I N G S -- --
 
-        // Initialisation of modules
-        DrumBlock.init();
-        Transformation.init();
+        // Module Configuration
+        var config = {
 
-        launchTest();
-    };
+            flow : {
+                quarter : 1,
+                eighth : 2,
+                triplet : 3,
+                sixteenth : 4,
+                quintuplet : 5,
+                sextuplet : 6,
+                thirtySecond : 8,
+            },
 
-    /*
-        Lancement des tests de développement en cours
-     */
-    function launchTest() {
+            /*
+                Liste des rudiments de la batterie
+             */
+            rudiment : {
+                "singleStroke" : {
+                    "fingering" : ["A"],
+                },
+                "doubleStroke" : {
+                    "fingering" : ["A,A"],
+                },
+                "paradiddle" : {
+                    "fingering" : ["A,B,A,A,B,A,B,B"],
+                },
+                "paradiddlediddle" : {
+                    "fingering" : ["A,B,A,A,B,B"],
+                },
+                "paraparadiddle" : {
+                    "fingering" : ["A,B,A,B,A,A,B,A,B,A,B,B"],
+                },
+            },
 
-        //addAccent();
-        //getNoteFromBar();
-        conversion();
-        //ostinatiList();
-    }
+            /*
+                Liste des types de frappe
+            */
+            note : {
+                EMPTY : 0,
+                NORMAL : 1,
+                GHOST : 2,
+                ACCENT : 3
+            },
+        };
 
-    function conversion() {
-        // Launch tests
-        var barBinary = new Bar([
-            [0,1,2,3],
-            [1,0,0,1],
-            [1,0,0,1],
-            [8,9,10,11]
-        ]);
-        DrumBlock.drawBar(barBinary);
+        // Module name
+        var _name = "App";
 
+        // Module Method Exposition
+        var app = {
+            config : config,
+            init : init
+        };
 
-        var ternaryBar = Transformation.convertBarFlow(
-            barBinary,
-            self.config.flow.sixteenth,
-            self.config.flow.triplet,
-            Transformation.config.fillGapMode.empty);
-        DrumBlock.drawBar(ternaryBar, 0, 75);
-    };
+    // -- -- P R I V A T E   P R O P E R T I E S -- --
 
-    function ostinatiList() {
-        $.getJSON( "data/ostinati.binaire.json", function( ostinati ) {
-            console.log("j'ai récupéré le json !!");
-            DrumBlock.drawOstinatiList(ostinati);
-        });
-    };
+    // -- -- P U B L I C   M E T H O D S -- --
 
-    function getNoteFromBar() {
-        console.log("============ GET NOTE FROM BAR ============");
+        /*
+            Initialisation du module
+         */
+        function init()
+        {
+            log("Initialisation");
 
-        var bar = new Bar([
-            [0,1,2,3],
-            [1,0,0,1],
-            [1,0,0,1],
-            [8,9,10,11]
-        ]);
-        console.log("------ BAR ------");
-        console.log(bar);
+            // Initialisation of modules
+            DrumBlock.init();
+            Transformation.init();
+            Score.init();
+
+            launchTest();
+        };
+
+    // -- -- P R I V A T E   M E T H O D S -- --
 
 
-        console.log("------ Elements ------");
-        for (var i = 0; i < 16; i++) {
-            console.log("bar["+i+"] = " + bar.getNoteByIndex(i));
-            console.log(bar.convertIndexIntoCoordinates(i));
-            bar.setNoteByIndex(12, i);
+        /*
+            Log d'un message avec le nom du module
+         */
+        function log (message) {
+            console.log("["+_name+"] " + message);
+        };
+
+        /*
+            Lancement des tests de développement en cours
+         */
+        function launchTest() {
+
+            //addAccent();
+            //getNoteFromBar();
+            conversion();
+            //ostinatiList();
         }
-        console.log(bar);
 
-        console.log("============ end of GET NOTE FROM BAR ============");
-    }
+        function conversion() {
+            // Launch tests
+            var barBinary = new Bar([
+                [0,1,2,3],
+                [1,0,0,1],
+                [1,0,0,1],
+                [8,9,10,11]
+            ]);
+            DrumBlock.drawBar(barBinary);
 
-    function addAccent() {
-        var bar = new Bar([
-            [0,1,2,3],
-            [1,0,0,1],
-            [1,0,0,1],
-            [8,9,10,11]
-        ]);
-        var mask = new Bar([
-            [0,0,0,1],
-            [0,0,0,1],
-            [0,0,0,1],
-            [0,0,0,1]
-        ]);
 
-        console.log("==================================");
+            var ternaryBar = Transformation.convertBarFlow(
+                barBinary,
+                config.flow.sixteenth,
+                config.flow.triplet,
+                Transformation.config.fillGapMode.empty);
+            DrumBlock.drawBar(ternaryBar, 0, 75);
+        };
 
-        console.log("Intial values :");
-        console.log(bar.toString("bar"));
-        console.log(mask.toString("mask"));
+        function ostinatiList() {
+            $.getJSON( "data/ostinati.binaire.json", function( ostinati ) {
+                console.log("j'ai récupéré le json !!");
+                DrumBlock.drawOstinatiList(ostinati);
+            });
+        };
 
-        Transformation.addAccent(bar, 5, false);
-        console.log(bar.toString("bar (after accent on 5)"));
+        function getNoteFromBar() {
+            console.log("============ GET NOTE FROM BAR ============");
 
-        Transformation.addAccentFromMask(bar, mask);
-        console.log(bar.toString("bar (after accent on mask [ghost=true])"));
+            var bar = new Bar([
+                [0,1,2,3],
+                [1,0,0,1],
+                [1,0,0,1],
+                [8,9,10,11]
+            ]);
+            console.log("------ BAR ------");
+            console.log(bar);
 
-        console.log("==================================");
-    }
 
-})(app);
+            console.log("------ Elements ------");
+            for (var i = 0; i < 16; i++) {
+                console.log("bar["+i+"] = " + bar.getNoteByIndex(i));
+                console.log(bar.convertIndexIntoCoordinates(i));
+                bar.setNoteByIndex(12, i);
+            }
+            console.log(bar);
+
+            console.log("============ end of GET NOTE FROM BAR ============");
+        }
+
+        function addAccent() {
+            var bar = new Bar([
+                [0,1,2,3],
+                [1,0,0,1],
+                [1,0,0,1],
+                [8,9,10,11]
+            ]);
+            var mask = new Bar([
+                [0,0,0,1],
+                [0,0,0,1],
+                [0,0,0,1],
+                [0,0,0,1]
+            ]);
+
+            console.log("==================================");
+
+            console.log("Intial values :");
+            console.log(bar.toString("bar"));
+            console.log(mask.toString("mask"));
+
+            Transformation.addAccent(bar, 5, false);
+            console.log(bar.toString("bar (after accent on 5)"));
+
+            Transformation.addAccentFromMask(bar, mask);
+            console.log(bar.toString("bar (after accent on mask [ghost=true])"));
+
+            console.log("==================================");
+        }
+
+
+    return app;
+
+})();
