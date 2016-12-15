e965bf1f27acd96fef46fb1cd4096c6d16299859
mcharton <mcharton@hotmail.fr>
2016-12-15 15:49:27 +0100

    - Mise à jour des outils : Ajout de jsdoc + gulp-jsdoc3

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,75 @@
+/* ------------------------------------------------ */
+/* --- Dependencies --- */
+/* ------------------------------------------------ */
+
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var clean = require('gulp-clean');
 var watch = require('gulp-watch');
-
-gulp.task('default', ['babel'], function() {
-    console.log("Developper les taches gulp");
-});
-
-gulp.task('watch', function() {
-    gulp.watch("src/js/**/*", ["babel"]);
-});
+var jsdoc = require('gulp-jsdoc3');
 
 
 /* ------------------------------------------------ */
+/* --- Tasks --- */
+/* ------------------------------------------------ */
+
+
+//  G E N E R A L
+
+    // Default task
+    gulp.task('default', ['babel'], function() {
+        console.log("Developper les taches gulp");
+    });
+
+    // Watch the JS Files
+    gulp.task('watch', function() {
+        return gulp
+            .watch("src/js/**/*", ["babel"])
+            ;
+    });
+
+
+
+//  D O C U M E N T A T I O N
+
+    // Generate JS Documentation
+    gulp.task('doc', ['doc.clean'], function(cb) {
+        var config = require('./jsdoc.json');
+
+        gulp.src(['README.md', './src/js/**'], {read: false})
+            .pipe(jsdoc(config, cb))
+            ;
+    });
+
+    // Remove JS Documentation folder
+    gulp.task('doc.clean', function() {
+        return gulp
+            .src('./documentation/*')
+            .pipe(clean())
+            ;
+    });
+
+
+
 //  J S   T R A N S P I L A T I O N
-/* ------------------------------------------------ */
 
-// Remove compiled js folder
-gulp.task('babel.clean', function() {
-    return gulp
-        .src('src/compiled_js/*')
-        .pipe(clean());
-});
+    // Remove compiled js folder
+    gulp.task('babel.clean', function() {
+        return gulp
+            .src('src/compiled_js/*')
+            .pipe(clean())
+            ;
+    });
 
-// Transpile js files
-gulp.task('babel', ['babel.clean'], function() {
-    return gulp
-        .src('src/js/**/*.js')
-        .pipe(babel({
-            presets : ["latest"]
-        }))
-        .pipe(gulp.dest("src/compiled_js"));
-});
+    // Transpile js files
+    gulp.task('babel', ['babel.clean'], function() {
+        return gulp
+            .src('src/js/**/*.js')
+            .pipe(babel({
+                presets : ["latest"]
+            }))
+            .pipe(gulp.dest("src/compiled_js"))
+            ;
+    });
 
 
--- src/js/Block/BlockChop.js
+++ src/js/Block/BlockChop.js
@@ -9,17 +9,29 @@
 *
 */
 
-var BlockChop = function(richChop) {
-    var _name = "BlockChop";
-    var self = this;
+/**
+ * A chop within the block manager
+ *     Include graphic properties
+ * @class
+ */
+var BlockChop = (function(){
 
-    self.chop = chop;
-    self.position = 0;
+    var BlockChop = function(richChop) {
+        var _name = "BlockChop";
+        var self = this;
 
-    /*
-        Log d'un message avec le nom du module
-    */
-    self.log = function(message) {
-        console.log("["+_name+"] " + message);
+        self.chop = chop;
+        self.position = 0;
+
+        /*
+            Log d'un message avec le nom du module
+        */
+        self.log = function(message) {
+            console.log("["+_name+"] " + message);
+        };
     };
-};
+
+
+    return BlockChop;
+
+})();
